7f90da96a71baee80f9616830db0e9f495528223
WiktorWox <wiktor.w.boczek@gmail.com>
2022-09-20 23:49:24 +0200

    Dodano osiągnięcia

--- src/main/java/com/korona/koronaswiat/item/custom/AndurilWeapon.java
+++ src/main/java/com/korona/koronaswiat/item/custom/AndurilWeapon.java
@@ -2,10 +2,14 @@ package com.korona.koronaswiat.item.custom;
 
 import com.korona.koronaswiat.item.ModItemTier;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.LogicalSidedProvider;
 
 public class AndurilWeapon extends SwordItem {
     public AndurilWeapon(Properties properties) {
@@ -15,8 +19,12 @@ public class AndurilWeapon extends SwordItem {
     @Override
     public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         // Here we can add effects on hit with this weapon to the target
-        attacker.addEffect(new EffectInstance(Effects.BLINDNESS, 60));
-        target.addEffect(new EffectInstance(Effects.WITHER, 100, 1));
+        target.addEffect(new EffectInstance(Effects.BLINDNESS, 30));
+        target.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 30, 5));
+        target.addEffect(new EffectInstance(Effects.WEAKNESS, 30, 3));
+        MinecraftServer source = LogicalSidedProvider.INSTANCE.get(LogicalSide.SERVER);
+        source.getCommands().performCommand(source.createCommandSourceStack(), "title " + target.getName() + " title \"Jesteś ogłuszony!\"");
+        source.getCommands().performCommand(source.createCommandSourceStack(), "say hi");
         return super.hurtEnemy(stack, target, attacker);
     }
 }
