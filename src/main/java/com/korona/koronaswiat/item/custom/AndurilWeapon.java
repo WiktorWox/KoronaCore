edbb458179bcd6b1cfdd4325b8bfe92d754ee1c8
WiktorWox <wiktor.w.boczek@gmail.com>
2022-09-23 21:15:32 +0200

    Zaktualizowano Andurila

--- src/main/java/com/korona/koronaswiat/item/custom/AndurilWeapon.java
+++ src/main/java/com/korona/koronaswiat/item/custom/AndurilWeapon.java
@@ -1,15 +1,23 @@
 package com.korona.koronaswiat.item.custom;
 
+import com.google.common.collect.ImmutableMultimap;
 import com.korona.koronaswiat.item.ModItemTier;
+import com.korona.koronaswiat.util.ModSoundEvent;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.SoundCategory;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.LogicalSidedProvider;
+
+import java.util.Random;
 
 public class AndurilWeapon extends SwordItem {
     public AndurilWeapon(Properties properties) {
@@ -19,12 +27,18 @@ public class AndurilWeapon extends SwordItem {
     @Override
     public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         // Here we can add effects on hit with this weapon to the target
-        target.addEffect(new EffectInstance(Effects.BLINDNESS, 30));
-        target.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 30, 5));
-        target.addEffect(new EffectInstance(Effects.WEAKNESS, 30, 3));
         MinecraftServer source = LogicalSidedProvider.INSTANCE.get(LogicalSide.SERVER);
-        source.getCommands().performCommand(source.createCommandSourceStack(), "title " + target.getName() + " title \"Jesteś ogłuszony!\"");
-        source.getCommands().performCommand(source.createCommandSourceStack(), "say hi");
+        Random randomNumber = new Random(); //instance of random class
+        int int_random = randomNumber.nextInt(5);
+        if (int_random == 0) {
+            source.getCommands().performCommand(source.createCommandSourceStack(), "title " + target.getName().getString() + " title \"Jesteś ogłuszony!\"");
+            target.addEffect(new EffectInstance(Effects.BLINDNESS, 30, 1, true, true));
+            target.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 30, 5, true, true));
+            target.addEffect(new EffectInstance(Effects.WEAKNESS, 30, 3, true, true));
+            attacker.level.playSound((PlayerEntity)null, attacker.getX(), attacker.getY(), attacker.getZ(), ModSoundEvent.ITEM_SCYTHE_HIT.get(), SoundCategory.PLAYERS, 18, 1);
+        }
+
+        source.getCommands().performCommand(source.createCommandSourceStack(), "say Zaatakowano gracza "+ target.getName().getString());
         return super.hurtEnemy(stack, target, attacker);
     }
 }
