192cdbd0be3f8e5b72df833a18d7102f61de4478
WiktorWox <wiktor.w.boczek@gmail.com>
2022-10-25 21:48:50 +0200

    Added Teas and optimalized Items

--- src/main/java/com/korona/koronaswiat/item/ModItems.java
+++ src/main/java/com/korona/koronaswiat/item/ModItems.java
@@ -14,6 +14,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
+
     public static final DeferredRegister<Item> ITEMS
             = DeferredRegister.create(ForgeRegistries.ITEMS, KoronaSwiat.MOD_ID);
 
@@ -23,19 +24,50 @@ public class ModItems {
 
     // Dodane przedmioty:
     public static final RegistryObject<Item> CORONACOIN = ITEMS.register("coronacoin",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+            () -> new Item(new Item.Properties()
+                    .tab(ItemGroup.TAB_MISC)));
+
     public static final RegistryObject<Item> STRANGE_BREAD = ITEMS.register("strange_bread",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD).food(new Food.Builder().effect(() -> new EffectInstance(Effects.DIG_SLOWDOWN, 600, 3), 1.0F).effect(() -> new EffectInstance(Effects.CONFUSION, 600, 3), 1.0F).effect(() -> new EffectInstance(Effects.WATER_BREATHING, 600, 3), 1.0F).effect(() -> new EffectInstance(Effects.BLINDNESS, 600, 3), 1.0F).effect(() -> new EffectInstance(Effects.HUNGER, 600, 3), 1.0F).effect(() -> new EffectInstance(Effects.WEAKNESS, 600, 3), 1.0F).effect(() -> new EffectInstance(Effects.POISON, 600, 3), 1.0F).effect(() -> new EffectInstance(Effects.BAD_OMEN, 36000, 0), 0.15F).saturationMod(0.8F).nutrition(5).alwaysEat().build())));
+            () -> new Item(new Item.Properties()
+                    .tab(ItemGroup.TAB_FOOD)
+                    .food(new Food.Builder()
+                            .effect(() -> new EffectInstance(Effects.DIG_SLOWDOWN, 600, 3), 1.0F)
+                            .effect(() -> new EffectInstance(Effects.CONFUSION, 600, 3), 1.0F)
+                            .effect(() -> new EffectInstance(Effects.WATER_BREATHING, 600, 3), 1.0F)
+                            .effect(() -> new EffectInstance(Effects.BLINDNESS, 600, 3), 1.0F)
+                            .effect(() -> new EffectInstance(Effects.HUNGER, 600, 3), 1.0F)
+                            .effect(() -> new EffectInstance(Effects.WEAKNESS, 600, 3), 1.0F)
+                            .effect(() -> new EffectInstance(Effects.POISON, 600, 3), 1.0F)
+                            .effect(() -> new EffectInstance(Effects.BAD_OMEN, 36000, 0), 0.15F)
+                            .saturationMod(0.8F)
+                            .nutrition(5)
+                            .alwaysEat()
+                            .build())));
+    public static final RegistryObject<Item> TEA_EARL_GREY = ITEMS.register("tea_earl_grey",
+            () -> new Tea());
+    public static final RegistryObject<Item> TEA_YUNNAN = ITEMS.register("tea_yunnan",
+            () -> new Tea());
+    public static final RegistryObject<Item> TEA_YERBA = ITEMS.register("tea_yerba",
+            () -> new Tea());
+    public static final RegistryObject<Item> TEA_GREEN = ITEMS.register("tea_green",
+            () -> new Tea());
     public static final RegistryObject<Item> SOUL = ITEMS.register("soul",
             () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
     public static final RegistryObject<Item> SOUL_SAND_DUST = ITEMS.register("soul_sand_dust",
             () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
     public static final RegistryObject<Item> SOUL_INGOT = ITEMS.register("soul_ingot",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+            () -> new Item(new Item.Properties()
+                    .tab(ItemGroup.TAB_MISC)));
+
     public static final RegistryObject<Item> SCYTHE = ITEMS.register("scythe",
-            () -> new ScytheWeapon(new Item.Properties().tab(ItemGroup.TAB_COMBAT).stacksTo(1)));
+            () -> new ScytheWeapon(new Item.Properties()
+                    .tab(ItemGroup.TAB_COMBAT).stacksTo(1)));
+
     public static final RegistryObject<Item> ANDURIL = ITEMS.register("anduril",
-            () -> new AndurilWeapon(new Item.Properties().tab(ItemGroup.TAB_COMBAT).stacksTo(1)));
+            () -> new AndurilWeapon(new Item.Properties()
+                    .tab(ItemGroup.TAB_COMBAT)
+                    .stacksTo(1)));
     public static final RegistryObject<Item> MEGACORONACOIN = ITEMS.register("megacoronacoin",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+            () -> new Item(new Item.Properties()
+                    .tab(ItemGroup.TAB_MISC)));
 }
