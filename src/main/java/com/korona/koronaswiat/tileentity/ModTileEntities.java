1fbe22553ce21b8f6304894f265a78d078adf7a0
WiktorWox <wiktor.w.boczek@gmail.com>
2023-03-14 01:08:35 +0100

    Added registry of disc tile entity | Block - Disc - Update

--- src/main/java/com/korona/koronaswiat/tileentity/ModTileEntities.java
+++ src/main/java/com/korona/koronaswiat/tileentity/ModTileEntities.java
@@ -29,6 +29,10 @@ public class ModTileEntities {
             TILE_ENTITIES.register("heart_of_the_base_tile", () -> TileEntityType.Builder.of(
                     HeartOfTheBaseTile::new, Blocks.COBBLESTONE).build(null));
 
+    public static RegistryObject<TileEntityType<DiscTile>> DISC_TILE =
+            TILE_ENTITIES.register("disc_tile", () -> TileEntityType.Builder.of(
+                    DiscTile::new, Blocks.COBBLESTONE).build(null));
+
     public static void register(IEventBus eventBus) {
         TILE_ENTITIES.register(eventBus);
     }
