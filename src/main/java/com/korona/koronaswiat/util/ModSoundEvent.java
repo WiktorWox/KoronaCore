c21d051a9ae472399ed8107c971360e736071108
WiktorWox <wiktor.w.boczek@gmail.com>
2023-03-14 01:13:03 +0100

    Added some NPCs sounds | Mechanics - Sounds - Update
    Added announcement and some other dialogue lines

--- src/main/java/com/korona/koronaswiat/util/ModSoundEvent.java
+++ src/main/java/com/korona/koronaswiat/util/ModSoundEvent.java
@@ -20,7 +20,44 @@ public class ModSoundEvent {
             registerSoundEvent("item_soul_pickaxe_discharge");
     public static final RegistryObject<SoundEvent> ITEM_BANNER =
             registerSoundEvent("item_banner");
-
+    public static final RegistryObject<SoundEvent> KYLE =
+            registerSoundEvent("kyle");
+    public static final RegistryObject<SoundEvent> LUIS_ABOUTPLACE =
+            registerSoundEvent("luis_aboutplace");
+    public static final RegistryObject<SoundEvent> LUIS_ATTACK =
+            registerSoundEvent("luis_attack");
+    public static final RegistryObject<SoundEvent> LUIS_IFTELLSOMEONE =
+            registerSoundEvent("luis_iftellsomeone");
+    public static final RegistryObject<SoundEvent> LUIS_INTRO =
+            registerSoundEvent("luis_intro");
+    public static final RegistryObject<SoundEvent> LUIS_INTRO2 =
+            registerSoundEvent("luis_intro2");
+    public static final RegistryObject<SoundEvent> LUIS_NEWS =
+            registerSoundEvent("luis_news");
+    public static final RegistryObject<SoundEvent> LUIS_STRUCTURE =
+            registerSoundEvent("luis_structure");
+    public static final RegistryObject<SoundEvent> LUIS_WHYHIDING =
+            registerSoundEvent("luis_whyhiding");
+    public static final RegistryObject<SoundEvent> DISC =
+            registerSoundEvent("disc");
+    public static final RegistryObject<SoundEvent> MESSAGE =
+            registerSoundEvent("message");
+    public static final RegistryObject<SoundEvent> ANNOUCEMENT1 =
+            registerSoundEvent("annoucement1");
+    public static final RegistryObject<SoundEvent> ANNOUCEMENT2 =
+            registerSoundEvent("annoucement2");
+    public static final RegistryObject<SoundEvent> ANNOUCEMENT3 =
+            registerSoundEvent("annoucement3");
+    public static final RegistryObject<SoundEvent> ANNOUCEMENT4 =
+            registerSoundEvent("annoucement4");
+    public static final RegistryObject<SoundEvent> ANNOUCEMENT5 =
+            registerSoundEvent("annoucement5");
+    public static final RegistryObject<SoundEvent> ANNOUCEMENT6 =
+            registerSoundEvent("annoucement6");
+    public static final RegistryObject<SoundEvent> ANNOUCEMENT7 =
+            registerSoundEvent("annoucement7");
+    public static final RegistryObject<SoundEvent> ANNOUCEMENT8 =
+            registerSoundEvent("annoucement8");
     private static RegistryObject<SoundEvent> registerSoundEvent(String name) {
         return SOUND_EVENTS.register(name, () -> new SoundEvent(new ResourceLocation(KoronaSwiat.MOD_ID, name)));
     }
